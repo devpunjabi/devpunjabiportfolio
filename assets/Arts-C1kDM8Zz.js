85e251aaac739942c0c851dc994a56094f46eace
dev.punjabi <dev.punjabi@kit.edu>
2026-08-10 11:32:54 +0200

    Updates

--- assets/Arts-C1kDM8Zz.js
+++ assets/Arts-C1kDM8Zz.js
@@ -1,4 +1,4 @@
-import{c,u as _,r as x,j as e,I as N,X as $,R as Z,L as G,A as M}from"./index-CAz9iqVP.js";/**
+import{c,u as _,r as x,j as e,I as N,X as $,R as Z,L as G,A as M}from"./index-CDSZ67FH.js";/**
  * @license lucide-react v0.554.0 - ISC
  *
  * This source code is licensed under the ISC license.
